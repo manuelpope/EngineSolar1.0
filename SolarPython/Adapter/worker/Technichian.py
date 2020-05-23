d3b8370dc3463437ad93544395eddeacb9ce4232
manueltobon <manuelpope@gmail.com>
2020-05-23 14:25:34 -0300

    "made it able to run"

--- SolarPython/Adapter/worker/Technichian.py
+++ SolarPython/Adapter/worker/Technichian.py
@@ -1,11 +1,13 @@
 # Use all data from Reader ,
 # create electric schematics building loads and
 # listing them. Storing all Loads in DB table.
+
 from Adapter.action.ReaderInstrument import ReaderInstrument
 from Adapter.action.BuilderLoad import BuilderLoad
 
 
-readerx = ReaderInstrument("/Users/manueltobon/SolarPython/jsonTestData.json")
-builder = BuilderLoad(readerx)
-print(builder.buildLoad())
-print(builder.__str__())
+if __name__ == '__main__':
+    readerx = ReaderInstrument("/Users/manueltobon/SolarPython/jsonTestData.json")
+    builder = BuilderLoad(readerx)
+    print(builder.buildLoad())
+    print(builder.__str__())
