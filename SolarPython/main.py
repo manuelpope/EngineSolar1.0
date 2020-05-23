0de6dc5a6cc4af54c1b923393cf17aec8b7662be
manueltobon <manuelpope@gmail.com>
2020-05-23 17:41:48 -0300

    "made it able to run"

--- SolarPython/main.py
+++ SolarPython/main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
-import json , os
 
-# initializations
-app = Flask(__name__)
+import json, os
+
+
+
 def processJsonReader(pathProp = ""):
     dir = os.path.dirname(__file__)
     filename = os.path.join(dir, pathProp)
@@ -11,6 +12,8 @@ def processJsonReader(pathProp = ""):
         dictLoad = json.load(f)
     return dictLoad
 dictProp = processJsonReader("properties.json")
+# initializations
+app = Flask(__name__)
 # Mysql Connection
 app.config['MYSQL_HOST'] = dictProp.get('MYSQL_HOST')
 app.config['MYSQL_USER'] = dictProp.get('MYSQL_USER')
@@ -21,8 +24,6 @@ mysql = MySQL(app)
 app.secret_key = "mysecretkey"
 
 
-
-
 if __name__=='__main__':
 
-    app.run(debug=True)
+    app.run(port='8000',debug=True)
